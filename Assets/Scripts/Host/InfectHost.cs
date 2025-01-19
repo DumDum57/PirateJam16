020c1c4a3a4039fad5912afa20ed2c986795de21
Andrew <andrewragnarok77@gmail.com>
2025-01-18 17:41:06 -0700

    can select dot

--- Assets/Scripts/Host/InfectHost.cs
+++ Assets/Scripts/Host/InfectHost.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InfectHost : MonoBehaviour
 {
     public Sprite normalSprite;
     public Sprite hoveredSprite;
+
+    private bool selected = false;
+    private bool held = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,16 +22,34 @@ public class InfectHost : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && selected && !held)
+        {
+            selected = false;
+            GetComponent<SpriteRenderer>().sprite = normalSprite;
+            Debug.Log("sex");
+        }
+        else if (!Input.GetMouseButtonDown(0) && held)
+        {
+            held = false;
+        }
     }
 
     private void OnMouseOver()
     {
-        GetComponent<SpriteRenderer>().sprite = hoveredSprite;
+        if (!selected)
+            GetComponent<SpriteRenderer>().sprite = hoveredSprite;
+
+        if (Input.GetMouseButtonDown(0) && !selected && !held)
+        {
+            selected = true;
+            held = true;
+            GetComponent<SpriteRenderer>().sprite = hoveredSprite;
+        }
     }
 
     private void OnMouseExit()
     {
-        GetComponent<SpriteRenderer>().sprite = normalSprite;
+        if (!selected)
+            GetComponent<SpriteRenderer>().sprite = normalSprite;
     }
 }
